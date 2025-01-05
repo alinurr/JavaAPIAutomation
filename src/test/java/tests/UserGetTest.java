a8bbdd71108dccc0a07aa560d572df3e4f85944a
Raiymbekova Alinur <alinurraiymbekova@gmail.com>
2025-01-05 15:00:42 +0500

    test get user details with authorized user

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -6,6 +6,9 @@ import lib.BaseTestCase;
 import lib.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserGetTest extends BaseTestCase {
     @Test
     public void testGetUserDataNotAuth(){
@@ -13,9 +16,35 @@ public class UserGetTest extends BaseTestCase {
                 .get("https://playground.learnqa.ru/api/user/2")
                 .andReturn();
 
-        Assertions.assertJsonHasKey(responseUserData, "username");
-        Assertions.assertJsonHasNotKey(responseUserData, "firstName");
-        Assertions.assertJsonHasNotKey(responseUserData, "lastName");
-        Assertions.assertJsonHasNotKey(responseUserData, "email");
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotField(responseUserData, "firstName");
+        Assertions.assertJsonHasNotField(responseUserData, "lastName");
+        Assertions.assertJsonHasNotField(responseUserData, "email");
+    }
+
+    @Test
+    public void testGetUserDetailsAuthAsSameUser(){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        Response responseUserData = RestAssured
+                .given()
+                .header("x-csrf-token", header)
+                .cookie("auth_sid", cookie)
+                .get("https://playground.learnqa.ru/api/user/2")
+                .andReturn();
+
+        String[] expectedFields = {"username", "firstName", "lastName", "email"};
+        Assertions.assertJsonHasFields(responseUserData, expectedFields);
     }
 }
