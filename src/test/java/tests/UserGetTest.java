e031950ffa7d0f8d55df2e2fd336b8746b95af07
Raiymbekova Alinur <alinurraiymbekova@gmail.com>
2025-01-13 10:58:31 +0500

    homework test auth with one user and get detail of another user by requesting with another userId

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -47,4 +47,32 @@ public class UserGetTest extends BaseTestCase {
         String[] expectedFields = {"username", "firstName", "lastName", "email"};
         Assertions.assertJsonHasFields(responseUserData, expectedFields);
     }
+
+    @Test
+    public void testAuthWithOneUserGetAnotherUserDetail(){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        Response responseUserData = RestAssured
+                .given()
+                .header("x-csrf-token", header)
+                .cookie("auth_sid", cookie)
+                .get("https://playground.learnqa.ru/api/user/3")
+                .andReturn();
+
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotField(responseUserData, "firstName");
+        Assertions.assertJsonHasNotField(responseUserData, "lastName");
+        Assertions.assertJsonHasNotField(responseUserData, "email");
+    }
 }
